81b808b389f3f24cfbf2f8df62b812247fa11187
zhy <hyzhangzhy@outlook.com>
2025-12-16 01:07:44 +0800

    add

--- leetcode/2141/main.go
+++ leetcode/2141/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "fmt"
 
+func maxRunTime(n int, batteries []int) int64 {
+	l := 1
+	r := 0
+	nb := len(batteries)
+	for i := 0; i < nb; i++ {
+		r += batteries[i]
+	}
+	check := func(x int) bool {
+		sum := 0
+		for i := 0; i < nb; i++ {
+			sum += min(batteries[i], x)
+		}
+		return sum >= n*x
+	}
+	for l <= r {
+		mid := (l + r) / 2
+		if check(mid) {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+
+	}
+	return int64(l - 1)
+}
+
+func main() {
+	fmt.Println(maxRunTime(2, []int{3, 3, 3}))
 }
